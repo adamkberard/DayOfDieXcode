a72275ad3a1e3a81a264004a55a2533cf936f410
Adam <adamkberard@gmail.com>
2021-09-22 23:31:34 -0700

    player search uses base table

--- DayOfDie/Player/PlayerSearch/PlayerSearchViewController.swift
+++ DayOfDie/Player/PlayerSearch/PlayerSearchViewController.swift
@@ -7,116 +7,27 @@
 
 import UIKit
 
-class PlayerSearchViewController: UITableViewController, UISearchResultsUpdating {
-    
-    var playerList : [Player] = []
-    var filteredPlayerList : [Player] = []
-    let searchController = UISearchController(searchResultsController: nil)
-    var selectedPlayer : Player?
-    
-    private let myRefreshControl = UIRefreshControl()
-    
-    var isFiltering: Bool {
-      return searchController.isActive && !isSearchBarEmpty
-    }
-    var isSearchBarEmpty: Bool {
-      return searchController.searchBar.text?.isEmpty ?? true
-    }
+class PlayerSearchViewController: SearchTableViewController<Player> {
     
     override func viewDidLoad() {
+        setObjectList()
+        cellIdentifier = "PlayerCell"
+        tableSegueIdentifier = "toPlayerDetail"
+        fetchURLPostfix = "/players/"
+        searchPlaceholderString = "Fetching Player Data..."
         super.viewDidLoad()
-        tableView.register(UINib(nibName: "PlayerCell", bundle: nil), forCellReuseIdentifier: "PlayerCell")
-        
-        searchController.searchResultsUpdater = self
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.placeholder = "Search Players"
-        navigationItem.searchController = searchController
-        definesPresentationContext = true
-        
-        
-        tableView.refreshControl = myRefreshControl
-        myRefreshControl.addTarget(self, action: #selector(refreshUserList(_:)), for: .valueChanged)
-        myRefreshControl.attributedTitle = NSAttributedString(string: "Fetching Player Data...")
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        setPlayerList()
+        super.viewWillAppear(animated)
+        setObjectList()
         tableView.reloadData()
     }
     
-    func setPlayerList() -> Void {
-        playerList = Player.allPlayers.filter { (player: Player) -> Bool in
+    func setObjectList() -> Void {
+        objectList = Player.allPlayers.filter { (player: Player) -> Bool in
             return player != User.player
         }
-    }
-    
-    func filterContentForSearchText(_ searchText: String) {
-      filteredPlayerList = playerList.filter { (player: Player) -> Bool in
-        return player.username.lowercased().contains(searchText.lowercased())
-      }
-      tableView.reloadData()
-    }
-    
-    @objc func refreshUserList(_ sender: Any) {
-        fetchUserData()
-    }
-    
-    func fetchUserData() {
-        // Loading all users here
-        APICalls.getUsers {status, returnData in
-            if status{
-                Player.allPlayers = returnData as! [Player]
-                self.setPlayerList()
-                self.tableView.reloadData()
-                self.myRefreshControl.endRefreshing()
-            }
-            else{
-                let errors : [String] = returnData as! [String]
-                print(errors)
-            }
-        }
-    }
-    
-    func updateSearchResults(for searchController: UISearchController) {
-        let searchBar = searchController.searchBar
-        filterContentForSearchText(searchBar.text!)
-    }
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if isFiltering {
-            return filteredPlayerList.count
-        }
-        return playerList.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellIdentifier = "PlayerCell"
-        
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? PlayerCell  else {
-            fatalError("The dequeued cell is not an instance of PlayerCell.")
-        }
-        
-        if isFiltering {
-            cell.setupCell(object: filteredPlayerList[indexPath.row])
-        }
-        else {
-            cell.setupCell(object: playerList[indexPath.row])
-        }
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if isFiltering {
-            selectedPlayer = filteredPlayerList[indexPath.row]
-        }
-        else {
-            selectedPlayer = playerList[indexPath.row]
-        }
-        self.performSegue(withIdentifier: "toPlayerDetail", sender: self)
     }
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -126,7 +37,7 @@ class PlayerSearchViewController: UITableViewController, UISearchResultsUpdating
             if identifier == "toPlayerDetail" {
                 guard let viewController = segue.destination as? PlayerViewController else {
                     fatalError("Unexpected destination: \(segue.destination)")}
-                viewController.player = selectedPlayer
+                viewController.player = selectedObject
             }
         }
     }
